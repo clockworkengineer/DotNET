ba59fa3e63bdee496842f1f64971e64d3113ea2e
Rob Tizzard <robert_tizzard@hotmail.com>
2019-07-01 17:57:04 +0100

    Init

---
+++ GameOfLife/GameOfLifeConsoleText/ConwaysGameOfLifeConsole.cs
@@ -0,0 +1,39 @@
+﻿using GameOfLifeLibrary;
+using System;
+using System.Threading;
+
+namespace ConwaysGameOfLifeConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            CLifeText life = new CLifeText(Console.WindowHeight, Console.WindowWidth);
+
+            RandomizeGrid(life);
+
+            life.start();
+            while (!Console.KeyAvailable)
+            {
+                Thread.Sleep(200);
+                life.nextTick();
+            }
+            life.stop();
+
+            Console.Read();
+
+        }
+
+        private static void RandomizeGrid(CLifeText life) {
+
+            Random randomizer = new Random();
+
+            for (var y = 0; y < life.CellGridHeight; y++) {
+                for (var x = 0; x < life.CellGridWidth; x++) {
+                    life.setCell(y, x, Convert.ToBoolean((int)randomizer.Next(0, 2)));
+                }
+            }
+        }
+    }
+}
---
+++ GameOfLife/GameOfLifeLibrary/CLifeText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GameOfLifeLibrary
+{
+    public class CLifeText : CLife
+    {
+        public CLifeText(int height, int width) : base(height, width)
+        {
+        }
+
+        public override void updateCell(int y, int x, bool active)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(active ? '*' : ' ');
+        }
+    }
+}
