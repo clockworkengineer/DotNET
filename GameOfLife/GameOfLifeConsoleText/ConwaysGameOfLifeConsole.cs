29b7183a6144518a79dcd197d10358007fbaea0d
Rob Tizzard <robert_tizzard@hotmail.com>
2019-07-03 17:27:53 +0100

    Current snapshot.

--- GameOfLife/GameOfLifeConsoleText/ConwaysGameOfLifeConsole.cs
+++ GameOfLife/GameOfLifeConsoleText/ConwaysGameOfLifeConsole.cs
@@ -9,31 +9,59 @@ namespace ConwaysGameOfLifeConsole
         public static void Main(string[] args)
         {
 
-            CLifeText life = new CLifeText(Console.WindowHeight, Console.WindowWidth);
+            CLifeText cellGrid = new CLifeText(Console.WindowHeight-1, Console.WindowWidth);
 
-            RandomizeGrid(life);
+            Console.SetCursorPosition(0, Console.WindowHeight-1);
+            Console.Write("Commands: 1(Start) 2(Stop) 3(Reset) 4(Quit)");
 
-            life.start();
-            while (!Console.KeyAvailable)
+            RandomizeGrid(cellGrid);
+
+            while (processNextTick(cellGrid))
             {
+                cellGrid.nextTick();
                 Thread.Sleep(200);
-                life.nextTick();
             }
-            life.stop();
-
-            Console.Read();
 
         }
 
-        private static void RandomizeGrid(CLifeText life) {
+        private static void RandomizeGrid(CLifeText cellGrid) {
 
             Random randomizer = new Random();
 
-            for (var y = 0; y < life.CellGridHeight; y++) {
-                for (var x = 0; x < life.CellGridWidth; x++) {
-                    life.setCell(y, x, Convert.ToBoolean((int)randomizer.Next(0, 2)));
+            for (var y = 0; y < cellGrid.CellGridHeight; y++) {
+                for (var x = 0; x < cellGrid.CellGridWidth; x++) {
+                    cellGrid.setCell(y, x, Convert.ToBoolean((int)randomizer.Next(0, 2)));
                 }
             }
+        }
+
+        private static bool processNextTick(CLifeText cellGrid)
+        {
+            if (Console.KeyAvailable)
+            {
+
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        cellGrid.start();
+                        break;
+                    case '2':
+                        cellGrid.stop();
+                        break;
+                    case '3':
+                        cellGrid.stop();
+                        RandomizeGrid(cellGrid);
+                        cellGrid.Tick = 0;
+
+                        break;
+                    case '4':
+                        Console.Clear();
+                        return (false);
+                }
+            }
+
+            return (true);
+
         }
     }
 }
--- GameOfLife/GameOfLifeLibrary/CLife.cs
+++ GameOfLife/GameOfLifeLibrary/CLife.cs
@@ -1,26 +1,48 @@
-﻿using System;
+﻿//
+// Author: Robert Tizzard
+// 
+// Class: CLife
+//
+// Description: Base class implementation of Conway's game of life a cellular automaton.
+// 
+// Copyright 2018.
+//
+
+using System;
 
 namespace GameOfLifeLibrary
 {
     public class CLife
     {
-        private bool _running=false;
-        private int _tick = 0; 
-        private int _cellGridWidth = 0;
-        private int _cellGridHeight = 0;
-        private byte[] _cellMasterGrid;
-        private byte[] _cellGridReadOnly;
+        private bool _running=false;        // true then running automaton
+        private int _tick = 0;              // Current tick
+        private int _cellGridWidth = 0;     // Cell grid width
+        private int _cellGridHeight = 0;    // Cell grid height
+        private bool[] _cellMasterGrid;     // Master cell grid
+        private bool[] _cellGridReadOnly;   // Readonly copy of master cell grid
 
         public bool Running { get => _running; set => _running = value; }
         public int Tick { get => _tick; set => _tick = value; }
         public int CellGridWidth { get => _cellGridWidth; private set => _cellGridWidth = value; }
         public int CellGridHeight { get => _cellGridHeight; private set => _cellGridHeight = value; }
 
+        /// <summary>
+        /// Returns index of cell at y, x.
+        /// </summary>
+        /// <returns>The index.</returns>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="x">The x coordinate.</param>
         private int cellIndex(int y, int x)
         {
             return ((y * CellGridWidth) + x);
         }
 
+        /// <summary>
+        /// Perform bounds checking on coordinates and wrap them if necessary.
+        /// </summary>
+        /// <returns>The bounds.</returns>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="x">The x coordinate.</param>
         private Tuple<int, int> gridBounds(int y, int x)
         {
 
@@ -46,6 +68,12 @@ namespace GameOfLifeLibrary
 
         }
 
+        /// <summary>
+        /// Determine whether a grid cell is active.
+        /// </summary>
+        /// <returns><c>true</c>, if cell active was ised, <c>false</c> otherwise.</returns>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="x">The x coordinate.</param>
         private bool isCellActive(int y, int x)
         {
 
@@ -55,6 +83,12 @@ namespace GameOfLifeLibrary
 
         }
 
+        /// <summary>
+        /// Determine number of active neighbours of a given grid cell.
+        /// </summary>
+        /// <returns>The cell neighbours.</returns>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="x">The x coordinate.</param>
         private int activeCellNeighbours(int y, int x)
         {
 
@@ -101,15 +135,15 @@ namespace GameOfLifeLibrary
         {
             CellGridWidth = width;
             CellGridHeight = height;
-            _cellMasterGrid = new byte[CellGridWidth * CellGridHeight];
-            _cellGridReadOnly = new byte[CellGridWidth * CellGridHeight];
+            _cellMasterGrid = new bool[CellGridWidth * CellGridHeight];
+            _cellGridReadOnly = new bool[CellGridWidth * CellGridHeight];
         }
 
         public void setCell(int y, int x, bool active)
         {
             var coords = gridBounds(y, x);
 
-            _cellMasterGrid[cellIndex(coords.Item1, coords.Item2)] = Convert.ToByte(active);
+            _cellMasterGrid[cellIndex(coords.Item1, coords.Item2)] = active;
 
             updateCell(y, x, active);
         }
