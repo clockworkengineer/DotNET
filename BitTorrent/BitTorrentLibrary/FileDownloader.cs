1d8eb0f2f09790db1046772a347a3759059942d2
Rob Tizzard <robert_tizzard@hotmail.com>
2020-09-17 18:28:20 +0100

    Pass though Lint analyser.

--- BitTorrent/BitTorrentLibrary/FileDownloader.cs
+++ BitTorrent/BitTorrentLibrary/FileDownloader.cs
@@ -25,13 +25,11 @@ namespace BitTorrent
     /// </summary>
     public class FileDownloader
     {
-
-        private List<FileDetails> _filesToDownload; // Files in torrent to be dowloaded
-        private DownloadContext _dc;                // Torrent download context
-        private SHA1 _sha;                          // Object to create SHA1 piece info hash
-        private Task _pieceBufferWriterTask;        // Task for piece buffer writer 
-
-        public DownloadContext Dc { get => _dc; set => _dc = value; }
+        private readonly Object _pieceLock;                  // Piece Lock            
+        private readonly List<FileDetails> _filesToDownload; // Files in torrent to be dowloaded
+        private readonly SHA1 _sha;                          // Object to create SHA1 piece info hash
+        private readonly Task _pieceBufferWriterTask;        // Task for piece buffer writer 
+        public DownloadContext Dc { get; set; }              // Torrent download context
 
         /// <summary>
         /// Creates the empty files on diskas place holders of files to be downloaded.
@@ -50,11 +48,8 @@ namespace BitTorrent
                     {
                         fs.SetLength((Int64)file.length);
                     }
-
                 }
-
             }
-
         }
 
         /// <summary>
@@ -70,13 +65,12 @@ namespace BitTorrent
             UInt32 pieceOffset = pieceNumber * Constants.HashLength;
             for (var byteNumber = 0; byteNumber < Constants.HashLength; byteNumber++)
             {
-                if (hash[byteNumber] != _dc.piecesInfoHash[pieceOffset + byteNumber])
+                if (hash[byteNumber] != Dc.piecesInfoHash[pieceOffset + byteNumber])
                 {
-                    return (false);
+                    return false;
                 }
             }
-            return (true);
-
+            return true;
         }
 
         /// <summary>
@@ -87,39 +81,37 @@ namespace BitTorrent
         /// <param name="numberOfBytes">Number of bytes in piece.</param>
         private void GeneratePieceMapFromBuffer(UInt32 pieceNumber, byte[] pieceBuffer, UInt32 numberOfBytes)
         {
-
             bool pieceThere = CheckPieceHash(pieceNumber, pieceBuffer, numberOfBytes);
             if (pieceThere)
             {
-                _dc.totalBytesDownloaded += numberOfBytes;
+                Dc.totalBytesDownloaded += numberOfBytes;
             }
             UInt32 blockNumber = 0;
             for (; blockNumber < numberOfBytes / Constants.BlockSize; blockNumber++)
             {
-                _dc.BlockPieceLocal(pieceNumber, blockNumber, pieceThere);
-
+                Dc.BlockPieceLocal(pieceNumber, blockNumber, pieceThere);
             }
             if (numberOfBytes % Constants.BlockSize != 0)
             {
-                _dc.BlockPieceLocal(pieceNumber, numberOfBytes / Constants.BlockSize, pieceThere);
-                _dc.pieceMap[pieceNumber].lastBlockLength = numberOfBytes % Constants.BlockSize;
-                _dc.BlockPieceLast(pieceNumber, (numberOfBytes / Constants.BlockSize), true);
+                Dc.BlockPieceLocal(pieceNumber, numberOfBytes / Constants.BlockSize, pieceThere);
+                Dc.pieceMap[pieceNumber].lastBlockLength = numberOfBytes % Constants.BlockSize;
+                Dc.BlockPieceLast(pieceNumber, numberOfBytes / Constants.BlockSize, true);
             }
             else
             {
-                _dc.pieceMap[pieceNumber].lastBlockLength = Constants.BlockSize;
-                _dc.BlockPieceLast(pieceNumber, blockNumber - 1, true);
+                Dc.pieceMap[pieceNumber].lastBlockLength = Constants.BlockSize;
+                Dc.BlockPieceLast(pieceNumber, blockNumber - 1, true);
             }
         }
 
         /// <summary>
-        /// Creates the piece map from the current disc which details the state of the pieces 
-        /// within a torrentdownload. This could be whether a piece  is present on a remote peer, 
+        /// Creates the piece map from the current disc which details the state of the pieces
+        /// within a torrentdownload. This could be whether a piece  is present on a remote peer,
         /// has been requested or has already been downloaded/
         /// </summary>
         private void CreatePieceMap()
         {
-            byte [] pieceBuffer = new byte[_dc.pieceLength];
+            byte [] pieceBuffer = new byte[Dc.pieceLength];
             UInt32 pieceNumber = 0;
             UInt32 bytesInBuffer = 0;
             int bytesRead = 0;
@@ -132,22 +124,18 @@ namespace BitTorrent
 
                 using (var inFileSteam = new FileStream(file.name, FileMode.Open))
                 {
-
                     while ((bytesRead = inFileSteam.Read(pieceBuffer, (Int32)bytesInBuffer, pieceBuffer.Length - (Int32)bytesInBuffer)) > 0)
                     {
                         bytesInBuffer += (UInt32)bytesRead;
 
-                        if (bytesInBuffer == _dc.pieceLength)
+                        if (bytesInBuffer == Dc.pieceLength)
                         {
                             GeneratePieceMapFromBuffer(pieceNumber, pieceBuffer, bytesInBuffer);
                             bytesInBuffer = 0;
                             pieceNumber++;
                         }
-
                     }
-
                 }
-
             }
 
             if (bytesInBuffer > 0)
@@ -156,25 +144,24 @@ namespace BitTorrent
             }
 
             Log.Logger.Debug("Finished generating downloaded map.");
-
         }
 
         /// <summary>
-        /// Task to take a queued downloaed piece and write it away to the relevant file 
+        /// Task to take a queued downloaed piece and write it away to the relevant file
         /// sections to which it belongs.
         /// </summary>
         private void PieceBufferWriter()
         {
-            while(!_dc.pieceBufferWriteQueue.IsCompleted)
+            while(!Dc.pieceBufferWriteQueue.IsCompleted)
             {
-                PieceBuffer pieceBuffer = _dc.pieceBufferWriteQueue.Take();
+                PieceBuffer pieceBuffer = Dc.pieceBufferWriteQueue.Take();
 
-                if (CheckPieceHash(pieceBuffer.Number, pieceBuffer.Buffer, _dc.GetPieceLength(pieceBuffer.Number)))
+                if (CheckPieceHash(pieceBuffer.Number, pieceBuffer.Buffer, Dc.GetPieceLength(pieceBuffer.Number)))
                 {
                     Log.Logger.Trace($"writePieceToFiles({pieceBuffer.Number})");
 
-                    UInt64 startOffset = pieceBuffer.Number * _dc.pieceLength;
-                    UInt64 endOffset = startOffset + _dc.pieceLength;
+                    UInt64 startOffset = pieceBuffer.Number * Dc.pieceLength;
+                    UInt64 endOffset = startOffset + Dc.pieceLength;
 
                     foreach (var file in _filesToDownload)
                     {
@@ -185,7 +172,7 @@ namespace BitTorrent
                             using (Stream stream = new FileStream(file.name, FileMode.OpenOrCreate))
                             {
                                 stream.Seek((Int64)(startWrite - file.offset), SeekOrigin.Begin);
-                                stream.Write(pieceBuffer.Buffer, (Int32)(startWrite % _dc.pieceLength), (Int32)(endWrite - startWrite));
+                                stream.Write(pieceBuffer.Buffer, (Int32)(startWrite % Dc.pieceLength), (Int32)(endWrite - startWrite));
                                 stream.Flush();
                             }
                         }
@@ -195,10 +182,7 @@ namespace BitTorrent
                 {
                      Log.Logger.Error($"Error: Hash for piece {pieceBuffer.Number} is invalid.");
                 }
-
-              
             }
-
         }
 
         /// <summary>
@@ -209,9 +193,10 @@ namespace BitTorrent
         /// <param name="pieces">Pieces.</param>
         public FileDownloader(List<FileDetails> filesToDownload, UInt32 pieceLength, byte[] pieces)
         {
+            _pieceLock = new object();
             _filesToDownload = filesToDownload;
             _sha = new SHA1CryptoServiceProvider();
-            _dc = new DownloadContext(filesToDownload, pieceLength, pieces);
+            Dc = new DownloadContext(filesToDownload, pieceLength, pieces);
             _pieceBufferWriterTask = new Task(PieceBufferWriter);
             _pieceBufferWriterTask.Start();
         }
@@ -222,7 +207,7 @@ namespace BitTorrent
         /// </summary>
         ~FileDownloader()
         {
-            _dc.pieceBufferWriteQueue.CompleteAdding();
+            Dc.pieceBufferWriteQueue.CompleteAdding();
         }
 
         /// <summary>
@@ -230,7 +215,6 @@ namespace BitTorrent
         /// </summary>
         public void BuildDownloadedPiecesMap()
         {
-
             try
             {
                 CreateEmptyFilesOnDisk();
@@ -244,7 +228,6 @@ namespace BitTorrent
             {
                 Log.Logger.Debug(ex);
             }
-
         }
 
         /// <summary>
@@ -258,14 +241,13 @@ namespace BitTorrent
             {
                 Log.Logger.Trace($"havePiece({pieceNumber})");
 
-                for (UInt32 blockNumber = 0; blockNumber < _dc.blocksPerPiece; blockNumber++)
+                for (UInt32 blockNumber = 0; blockNumber < Dc.blocksPerPiece; blockNumber++)
                 {
-                    if (!_dc.IsBlockPieceLocal(pieceNumber, blockNumber))
+                    if (!Dc.IsBlockPieceLocal(pieceNumber, blockNumber))
                     {
-                        return (false);
+                        return false;
                     }
                 }
-
             }
             catch (Error)
             {
@@ -276,7 +258,7 @@ namespace BitTorrent
                 Log.Logger.Debug(ex);
             }
 
-            return (true);
+            return true;
         }
 
         /// <summary>
@@ -291,36 +273,35 @@ namespace BitTorrent
             {
                 // In onorder to stop same the piece requested with different peers a lock 
                 // is required when trying to get the next unrequested non-local piece
-                lock (this) 
+                lock (_pieceLock) 
                 {
                     Log.Logger.Trace($"selectNextPiece()");
 
-                    for (UInt32 pieceNumber = 0; pieceNumber < _dc.numberOfPieces; pieceNumber++)
+                    for (UInt32 pieceNumber = 0; pieceNumber < Dc.numberOfPieces; pieceNumber++)
                     {
                         if (remotePeer.IsPieceOnRemotePeer(pieceNumber))
                         {
                             UInt32 blockNumber = 0;
-                            for (; !_dc.IsBlockPieceLast(pieceNumber, blockNumber); blockNumber++)
+                            for (; !Dc.IsBlockPieceLast(pieceNumber, blockNumber); blockNumber++)
                             {
-                                if (!_dc.IsBlockPieceRequested(pieceNumber, blockNumber) &&
-                                    !_dc.IsBlockPieceLocal(pieceNumber, blockNumber))
+                                if (!Dc.IsBlockPieceRequested(pieceNumber, blockNumber) &&
+                                    !Dc.IsBlockPieceLocal(pieceNumber, blockNumber))
                                 {
                                     nextPiece = pieceNumber;
-                                    _dc.MarkPieceRequested(pieceNumber);
-                                    return (true);
+                                    Dc.MarkPieceRequested(pieceNumber);
+                                    return true;
                                 }
                             }
-                            if (!_dc.IsBlockPieceRequested(pieceNumber, blockNumber) &&
-                                !_dc.IsBlockPieceLocal(pieceNumber, blockNumber))
+                            if (!Dc.IsBlockPieceRequested(pieceNumber, blockNumber) &&
+                                !Dc.IsBlockPieceLocal(pieceNumber, blockNumber))
                             {
                                 nextPiece = pieceNumber;
-                                _dc.MarkPieceRequested(pieceNumber);
-                                return (true);
+                                Dc.MarkPieceRequested(pieceNumber);
+                                return true;
                             }
                         }
                     }
                 }
-
             }
             catch (Error)
             {
@@ -331,9 +312,7 @@ namespace BitTorrent
                 Log.Logger.Debug(ex);
             }
 
-            return (false);
-
+            return false;
         }
-
     }
 }
